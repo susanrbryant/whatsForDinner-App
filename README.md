89f8578ddd5811cec7f1c58539d6799f101d6211
Andrew J. Zdunek <andrewjzdunek@gmail.com>
2018-02-12 16:53:00 -0600

    adding to ReadMe

--- README.md
+++ README.md
@@ -1,42 +1,27 @@
 # whatsForDinner-App
 
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
-
->>>>>>> 1e5a89103752a76c33c510095b7351b095f05046
-# Project Title
+## About whatsForDinner-App
 
 Ever wonder what's for dinner? With the What's For Dinner app, you'll never find yourself searching for what to make when it's already past dinnertime. What's For Dinner pulls the top 30 recipes from popular food website Food2Fork to give you a recipe, appetizing photos, and customized shopping list. Pull up your recipe, stop into the grocery store, and dinner's already halfway made. You can even save your recipes for later use.
 
 ## Built With
 
-## Contributing
+- Bootstrap
+- CSS
+- jQuery
+- JavaScript
 
 ## Authors
+
+**Krista Jesek** (https://github.com/kjesek)
+**Susan Bryant** (https://github.com/susanrbryant)
+**Andrew Zdunek** (https://github.com/ajzdunek)
 
 ## License
 
 ## Acknowledgments
-<<<<<<< HEAD
-=======
-Description: 
 
-How to use:
-
-User Stories to be added:
-
-Collaborators:
-
-Date created:
-
-Tools used:
-
->>>>>>> 54ea10f7844d0b78db38c4597d6231562a45a127
-=======
-
-
-
->>>>>>> 1e5a89103752a76c33c510095b7351b095f05046
-
+-Hat tip to anyone who's code was used
+-Inspiration
+-etc
 
--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -10,48 +10,47 @@ var recipeArr = [];
 var search;
 
 
-(function(){
-  
+function save() {
+
     var list = document.querySelector('#list'),
         form = document.querySelector('form'),
         item = document.querySelector('#item');
-    
-    form.addEventListener('submit',function(e){
-      e.preventDefault();
-      list.innerHTML += '<li>' + item.value + '</li>';
-      store();
-      item.value = "";
-    },false)
-    
-    list.addEventListener('click',function(e){
-      var t = e.target;
-      if(t.classList.contains('checked')){
-        t.parentNode.removeChild(t);
-      } else {
-        t.classList.add('checked');
-      }
-      store();
-    },false)
-    
+
+    form.addEventListener('submit', function (e) {
+        e.preventDefault();
+        list.innerHTML += '<li>' + item.value + '</li>';
+        store();
+        item.value = "";
+    }, false);
+
+    list.addEventListener('click', function (e) {
+        var t = e.target;
+        if (t.classList.contains('checked')) {
+            t.parentNode.removeChild(t);
+        } else {
+            t.classList.add('checked');
+        }
+        store();
+    }, false);
+
     function store() {
-      window.localStorage.myitems = list.innerHTML;
+        window.localStorage.myitems = list.innerHTML;
     }
-    
+
     function getValues() {
-      var storedValues = window.localStorage.myitems;
-      if(!storedValues) {
-        list.innerHTML = '<li>Make a to do list</li>'+
-                         '<li>Check off first thing on the to do list</li>'+
-                         '<li>Realize you have already accomplished 2 things in the list</li>'+
-                         '<li>Reward yourself with a nap</li>';
-      }
-      else {
-        list.innerHTML = storedValues;
-      }
+        var storedValues = window.localStorage.myitems;
+        if (!storedValues) {
+            list.innerHTML = '<li>Make a to do list</li>' +
+                '<li>Check off first thing on the to do list</li>' +
+                '<li>Realize you have already accomplished 2 things in the list</li>' +
+                '<li>Reward yourself with a nap</li>';
+        }
+        else {
+            list.innerHTML = storedValues;
+        }
     }
     getValues();
-  });
-
+}
 
 function openNav() {
     document.getElementById("mySidenav").style.width = "250px";
@@ -66,19 +65,21 @@ function closeNav() {
 }
 
 function generateRecipe() {
+
     $("#randomButton").on("click", function () {
         // var queryURLOne = "http://food2fork.com/api/search?key=2a8b74ca359dd160bef9caeb0fa0ae5e&q";
-        // var queryURLTwo = "http://food2fork.com/api/get?key=2a8b74ca359dd160bef9caeb0fa0ae5e&q=" + recipesID;    
+        // var queryURLTwo = "http://food2fork.com/api/get?key=2a8b74ca359dd160bef9caeb0fa0ae5e&q=" + recipesID; 
         $.ajax({
             url: "https://cors-anywhere.herokuapp.com/" + "https://food2fork.com/api/search",
             type: "GET",
             data: {
                 key: "2a8b74ca359dd160bef9caeb0fa0ae5e",
+                sort: "t",
             },
             success: (function (result) {
                 recipesArr = JSON.parse(result);
                 randomArr = recipesArr.recipes;
-                randomArr.sort(function () { return 0.5 - Math.random() });
+                randomArr.sort(function () { return 0.5 - Math.random(); });
 
                 var recipesTitle = $("<p>");
                 var recipesImage = $("<img>");
@@ -106,46 +107,6 @@ function generateRecipe() {
                 }
                 image();
 
-                //     (function(){
-                //         var list = document.querySelector('#list'),
-                //         form = document.querySelector('form'),
-                //         item = document.querySelector('#item');
-
-                //     form.addEventListener('submit',function(e){
-                //       e.preventDefault();
-                //     list.innerHTML += '<li>' + item.value + recipesURL  + '</li>';
-                //       store();
-                //       item.value = "";
-                //     },false)
-
-                //     list.addEventListener('click',function(e){
-                //       var t = e.target;
-                //       if(t.classList.contains('checked')){
-                //         t.parentNode.removeChild(t);
-                //       } else {
-                //         t.classList.add('checked');
-                //       }
-                //       store();
-                //     },false)
-
-                //     function store() {
-                //       window.recipesURL.myitems = list.innerHTML;
-                //     }
-
-                //     function getValues() {
-                //       var storedValues = window.recipesURL.myitems;
-                //       if(!storedValues) {
-                //         list.innerHTML = '<li>Make a to do list</li>'+
-                //                          '<li>Check off first thing on the to do list</li>'+
-                //                          '<li>Realize you have already accomplished 2 things in the list</li>'+
-                //                          '<li>Reward yourself with a nap</li>';
-                //       }
-                //       else {
-                //         list.innerHTML = storedValues;
-                //       }
-                //     }
-                //     getValues();
-                //   })();
                 $("#ingrButton").on("click", function () {
                     $.ajax({
                         type: "GET",
@@ -162,20 +123,20 @@ function generateRecipe() {
 
                             $(".fas fa-heart").on("click", function () {
                                 // add recipe title and url to local storage nav bar
-                            })
+                            });
 
                             for (var i = 0; i < ingrArr.length; i++) {
-                                $("#fullGroceryList").append("<li>" + (i + 1) + ".  " + ingrArr[i] + "</li>");
+                                $("#fullGroceryList").append("<li>" + (i + 1) + ". " + ingrArr[i] + "</li>");
                                 var input = $("#savedGroceryList");
                                 localStorage.setItem("server", input.value);
-                            };
+                            }
 
                             $("#showGroceryList").on("click", function () {
                                 $("#showGroceryList").animate({ width: 0 }, { duration: 1000 });
                                 $("#showGroceryList").hide();
                                 $('#fullGroceryList').show();
                                 $('#fullGroceryList').animate({ width: 400 }, { duration: 1000 });
-                            })
+                            });
 
                             $("#fullGroceryList").click(function () {
                                 $(this).animate({ width: 0 }, { duration: 1000 });
@@ -187,7 +148,7 @@ function generateRecipe() {
                         error: (function (error) {
                             console.log("error: " + error);
                         })
-                    })
+                    });
                 });
             }),
         });
@@ -204,7 +165,7 @@ function generateRecipe() {
             success: (function (result) {
                 recipesArr = JSON.parse(result);
                 randomArr = recipesArr.recipes;
-                randomArr.sort(function () { return 0.5 - Math.random() });
+                randomArr.sort(function () { return 0.5 - Math.random(); });
 
                 var recipesTitle = $("<p>");
                 var recipesImage = $("<img>");
@@ -247,20 +208,20 @@ function generateRecipe() {
 
                             $(".fas fa-heart").on("click", function () {
                                 // add recipe title and url to local storage nav bar
-                            })
+                            });
 
                             for (var i = 0; i < ingrArr.length; i++) {
-                                $("#fullGroceryList").append("<li>" + (i + 1) + ".  " + ingrArr[i] + "</li>");
+                                $("#fullGroceryList").append("<li>" + (i + 1) + ". " + ingrArr[i] + "</li>");
                                 var input = $("#savedGroceryList");
                                 localStorage.setItem("server", input.value);
-                            };
+                            }
 
                             $("#showGroceryList").on("click", function () {
                                 $("#showGroceryList").animate({ width: 0 }, { duration: 1000 });
                                 $("#showGroceryList").hide();
                                 $('#fullGroceryList').show();
                                 $('#fullGroceryList').animate({ width: 400 }, { duration: 1000 });
-                            })
+                            });
 
                             $("#fullGroceryList").click(function () {
                                 $(this).animate({ width: 0 }, { duration: 1000 });
@@ -272,7 +233,7 @@ function generateRecipe() {
                         error: (function (error) {
                             console.log("error: " + error);
                         })
-                    })
+                    });
                 });
             }),
             error: (function (error) {
@@ -293,7 +254,7 @@ function generateRecipe() {
             success: (function (result) {
                 recipesArr = JSON.parse(result);
                 randomArr = recipesArr.recipes;
-                randomArr.sort(function () { return 0.5 - Math.random() });
+                randomArr.sort(function () { return 0.5 - Math.random(); });
 
                 var recipesTitle = $("<p>");
                 var recipesImage = $("<img>");
@@ -320,7 +281,7 @@ function generateRecipe() {
                     $('.container-recipe-image').html(img);
                 }
                 image();
-            
+
                 $("#ingrButton").on("click", function () {
                     $.ajax({
                         type: "GET",
@@ -337,20 +298,20 @@ function generateRecipe() {
 
                             $(".fas fa-heart").on("click", function () {
                                 // add recipe title and url to local storage nav bar
-                            })
+                            });
 
                             for (var i = 0; i < ingrArr.length; i++) {
-                                $("#fullGroceryList").append("<li>" + (i + 1) + ".  " + ingrArr[i] + "</li>");
+                                $("#fullGroceryList").append("<li>" + (i + 1) + ". " + ingrArr[i] + "</li>");
                                 var input = $("#savedGroceryList");
                                 localStorage.setItem("server", input.value);
-                            };
+                            }
 
                             $("#showGroceryList").on("click", function () {
                                 $("#showGroceryList").animate({ width: 0 }, { duration: 1000 });
                                 $("#showGroceryList").hide();
                                 $('#fullGroceryList').show();
                                 $('#fullGroceryList').animate({ width: 400 }, { duration: 1000 });
-                            })
+                            });
 
                             $("#fullGroceryList").click(function () {
                                 $(this).animate({ width: 0 }, { duration: 1000 });
@@ -362,7 +323,7 @@ function generateRecipe() {
                         error: (function (error) {
                             console.log("error: " + error);
                         })
-                    })
+                    });
                 });
             }),
         });
@@ -379,7 +340,7 @@ function generateRecipe() {
             success: (function (result) {
                 recipesArr = JSON.parse(result);
                 randomArr = recipesArr.recipes;
-                randomArr.sort(function () { return 0.5 - Math.random() });
+                randomArr.sort(function () { return 0.5 - Math.random(); });
 
                 var recipesTitle = $("<p>");
                 var recipesImage = $("<img>");
@@ -406,51 +367,8 @@ function generateRecipe() {
                     $('.container-recipe-image').html(img);
                 }
                 image();
+
                 $("#ingrButton").on("click", function () {
-            //     (function(){
-            //         var list = document.querySelector('#list'),
-            //         form = document.querySelector('form'),
-            //         item = document.querySelector('#item');
-                    
-            //     form.addEventListener('submit',function(e){
-            //       e.preventDefault();
-            //     list.innerHTML += '<li>' + item.value + recipesURL  + '</li>';
-            //       store();
-            //       item.value = "";
-            //     },false)
-                
-            //     list.addEventListener('click',function(e){
-            //       var t = e.target;
-            //       if(t.classList.contains('checked')){
-            //         t.parentNode.removeChild(t);
-            //       } else {
-            //         t.classList.add('checked');
-            //       }
-            //       store();
-            //     },false)
-                
-            //     function store() {
-            //       window.recipesURL.myitems = list.innerHTML;
-            //     }
-                
-            //     function getValues() {
-            //       var storedValues = window.recipesURL.myitems;
-            //       if(!storedValues) {
-            //         list.innerHTML = '<li>Make a to do list</li>'+
-            //                          '<li>Check off first thing on the to do list</li>'+
-            //                          '<li>Realize you have already accomplished 2 things in the list</li>'+
-            //                          '<li>Reward yourself with a nap</li>';
-            //       }
-            //       else {
-            //         list.innerHTML = storedValues;
-            //       }
-            //     }
-            //     getValues();
-            //   })();
-
-
-                // $("#ingrButton").on("click", function() 
-                $("#ingrButton").on("click", function() {
                     $.ajax({
                         type: "GET",
                         url: "https://cors-anywhere.herokuapp.com/" + "https://food2fork.com/api/get",
@@ -466,29 +384,26 @@ function generateRecipe() {
                             // $("#ingrTitle").toggleClass("hide");
                             // $("#ingrTitle").html("Ingredients: ");
 
-                            $(".fas fa-heart").on("click", function() {
+                            $(".fas fa-heart").on("click", function () {
                                 // add recipe title and url to local storage nav bar
-                            })
-
-                            $("#ingreTitle").toggleClass("hide");
-                            $("#ingrTitle").html("Ingredients: ");
+                            });
 
                             $(".fas fa-heart").on("click", function () {
                                 // add recipe title and url to local storage nav bar
-                            })
+                            });
 
                             for (var i = 0; i < ingrArr.length; i++) {
-                                $("#fullGroceryList").append("<li>" + (i + 1) + ".  " + ingrArr[i] + "</li>");
+                                $("#fullGroceryList").append("<li>" + (i + 1) + ". " + ingrArr[i] + "</li>");
                                 var input = $("#savedGroceryList");
                                 localStorage.setItem("server", input.value);
-                            };
+                            }
 
                             $("#showGroceryList").on("click", function () {
                                 $("#showGroceryList").animate({ width: 0 }, { duration: 1000 });
                                 $("#showGroceryList").hide();
                                 $('#fullGroceryList').show();
                                 $('#fullGroceryList').animate({ width: 400 }, { duration: 1000 });
-                            })
+                            });
 
                             $("#fullGroceryList").click(function () {
                                 $(this).animate({ width: 0 }, { duration: 1000 });
@@ -500,14 +415,10 @@ function generateRecipe() {
                         error: (function (error) {
                             console.log("error: " + error);
                         })
-                    })
+                    });
                 });
-            }),
-            error: (function (error) {
-                console.log(error);
             })
         });
-
     });
 
     $("#pastaButton").on("click", function () {
@@ -521,7 +432,7 @@ function generateRecipe() {
             success: (function (result) {
                 recipesArr = JSON.parse(result);
                 randomArr = recipesArr.recipes;
-                randomArr.sort(function () { return 0.5 - Math.random() });
+                randomArr.sort(function () { return 0.5 - Math.random(); });
 
                 var recipesTitle = $("<p>");
                 var recipesImage = $("<img>");
@@ -560,25 +471,23 @@ function generateRecipe() {
                         success: (function (result) {
                             recipeArr = JSON.parse(result);
                             ingrArr = recipeArr.recipe.ingredients;
-                            // $("#ingrTitle").toggleClass("hide");
-                            // $("#ingrTitle").html("Ingredients: ");
 
                             $(".fas fa-heart").on("click", function () {
                                 // add recipe title and url to local storage nav bar
-                            })
+                            });
 
                             for (var i = 0; i < ingrArr.length; i++) {
-                                $("#fullGroceryList").append("<li>" + (i + 1) + ".  " + ingrArr[i] + "</li>");
+                                $("#fullGroceryList").append("<li>" + (i + 1) + ". " + ingrArr[i] + "</li>");
                                 var input = $("#savedGroceryList");
                                 localStorage.setItem("server", input.value);
-                            };
+                            }
 
                             $("#showGroceryList").on("click", function () {
                                 $("#showGroceryList").animate({ width: 0 }, { duration: 1000 });
                                 $("#showGroceryList").hide();
                                 $('#fullGroceryList').show();
                                 $('#fullGroceryList').animate({ width: 400 }, { duration: 1000 });
-                            })
+                            });
 
 
                             $("#fullGroceryList").click(function () {
@@ -592,18 +501,16 @@ function generateRecipe() {
                         error: (function (error) {
                             console.log("error: " + error);
                         })
-                    })
+                    });
                 });
             }),
             error: (function (error) {
                 console.log(error);
             })
         });
-
     });
 }
 generateRecipe();
-
 
 $(window).scroll(function () {
     if ($(this).scrollTop() >= 100) {
@@ -623,6 +530,6 @@ anime.timeline({ loop: false }).add({
     easing: "easeInOutQuad",
     duration: 2250,
     delay: function (el, i) {
-        return 150 * (i + 1)
+        return 150 * (i + 1);
     }
 });
